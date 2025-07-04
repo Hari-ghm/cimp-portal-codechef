20af7edd5abbe9725ea19ded87c0c328d3215a95
Hari-ghm <hmadhav2006@gmail.com>
2025-07-04 19:54:22 +0530

    Set basename for GitHub Pages routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+// frontend/src/main.tsximport React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename="/cimp-portal-codechef/">
+      {" "}
+      {/* ✅ Important fix */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
