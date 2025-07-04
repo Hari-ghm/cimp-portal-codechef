574c5bccb2fc4c1970b8057125f40bb7b8e9e5cb
Hari-ghm <hmadhav2006@gmail.com>
2025-07-04 21:23:01 +0530

    made changes

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,19 @@ const cors = require("cors");
 require("dotenv").config();
 
 const clubRoutes = require("./routes/clubRoutes");
-const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-
-// ✅ CORS config – allow only your frontend
-app.use(
-  cors({
-    origin: "https://hari-ghm.github.io", // ← your deployed frontend URL
-    credentials: true, // optional; needed if using cookies or sessions
-  })
-);
-
+app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use("/api/clubs", clubRoutes);
-app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const authRoutes = require("./routes/authRoutes");
+app.use("/api", authRoutes);
+
