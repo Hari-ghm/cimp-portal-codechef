83245389a530ffb6488982edcd734add51696560
Hari-ghm <hmadhav2006@gmail.com>
2025-07-04 19:47:38 +0530

    Fix Vite base and GitHub Actions deploy config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({ 
+export default defineConfig({
+  base: "/cimp-portal-codechef/", // <-- Set to your GitHub repo name
   plugins: [react()],
 });
