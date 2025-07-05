5cd07375d4afc4b5a86103f07dd204eef69dafd7
Hari-ghm <hmadhav2006@gmail.com>
2025-07-05 06:42:17 +0530

    refreshed prolem solved

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,13 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import React from "react";
 
+
+const redirectPath = sessionStorage.getItem("redirectPath");
+if (redirectPath) {
+  sessionStorage.removeItem("redirectPath");
+  window.history.replaceState(null, "", redirectPath);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename="/cimp-portal-codechef/">
